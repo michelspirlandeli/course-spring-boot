c850f0f1ee0618b42ff0d0195172a3a0bb657208
michelspirlandeli <michelspirlandeli10@gmail.com>
2021-03-20 14:41:10 -0300

    Product

---
+++ src/main/java/com/michel/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.michel.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.michel.course.entities.Product;
+import com.michel.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository productRepository;
+	
+	public List<Product> findAll(){
+		return productRepository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = productRepository.findById(id);
+		return obj.get();
+	}
+}
