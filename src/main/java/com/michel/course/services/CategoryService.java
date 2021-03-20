4882c2d4365a8975085741dbcc6dd86bb467591a
michelspirlandeli <michelspirlandeli10@gmail.com>
2021-03-20 14:20:50 -0300

    Category

---
+++ src/main/java/com/michel/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.michel.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.michel.course.entities.Category;
+import com.michel.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public List<Category> findAll(){
+		return categoryRepository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = categoryRepository.findById(id);
+		return obj.get();
+	}
+}
